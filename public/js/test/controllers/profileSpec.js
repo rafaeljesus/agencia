307e2686c5f0a5a0d66e096083b9b2cf914ef22c
Carlos Eduardo Ferreiro Corrêa <kadusjc@yahoo.com.br>
2014-08-21 07:59:05 -0300

    Update profileSpec.js
    Avançando nos testes jasmines de Profile controller

--- public/js/test/controllers/profileSpec.js
+++ public/js/test/controllers/profileSpec.js
@@ -2,20 +2,49 @@
 
 describe('ProfileControllerSpec', function() {
 
-  var controller, scope, user, profile, location, http;
+  var controller, scope, user, profile, location, http, profileTransformer, defaultUser;
 
   beforeEach(function() {
     module('agencia');
+    
+    defaultUser = { 
+        primeiro_nome: 'userTest', 
+        sobrenome: 'reallyTest', 
+        email: 'valid@email.com',
+        altura: 1.75,
+        peso : 90;
+        trabalha : true,
+        estuda : true,
+        tem_filhos : true,
+        possui_carro : true,
+        como_mora : 'Moro sozinho(a)'
+        gosta_tv : true,
+        gosta_ler : true,
+        fuma : false,
+        pratica_exercicio : true,
+        sexo : '1',
+        como_atua : '1',
+        idade : 30,
+        compromissado: true,
+        cidade: 'São José dos Campos',
+        pais: 'BR',
+        estado: 'São Paulo', 
+        id : 1
+    };
   });
 
-  beforeEach(inject(function($rootScope, $controller, $location, _$httpBackend_, User, Profile) {
+  beforeEach(inject(function($rootScope, $controller, $location, _$httpBackend_, User, Profile, profileTransformer) {
     user = User;
     profile = Profile;
     scope = $rootScope.$new();
+    profileTransformer = profileTransformer;
     location = $location;
     http = _$httpBackend_;
     controller = $controller('ProfileController', {
-      $scope: scope
+      $scope: scope,
+      profileTransformer: profileTransformer,
+      User: user,
+      Profile: profile
     });
   }));
   
@@ -26,6 +55,14 @@ describe('ProfileControllerSpec', function() {
   
   
   it('should emit an event when load method is called', function(done) {
+    
+    scope.currentUser.id = 1;
+    
+    http.when('GET', '/user/1').respond(scope.user);
+    http.expectGET('/user/1').respond(200, scope.user);
+    scope.loadProfile();
+    http.flush();
+    done();
     
   });
 });
