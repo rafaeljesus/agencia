d4842b6ad2d3c7d7d4b51df352001e4c756d1bb5
Carlos Eduardo Ferreiro Corrêa <kadusjc@yahoo.com.br>
2014-08-21 07:42:21 -0300

    Update profileSpec.js

--- public/js/test/controllers/profileSpec.js
+++ public/js/test/controllers/profileSpec.js
@@ -1,1 +1,31 @@
-teste
+  var expect = chai.expect;
+
+describe('ProfileControllerSpec', function() {
+
+  var controller, scope, user, profile, location, http;
+
+  beforeEach(function() {
+    module('agencia');
+  });
+
+  beforeEach(inject(function($rootScope, $controller, $location, _$httpBackend_, User, Profile) {
+    user = User;
+    profile = Profile;
+    scope = $rootScope.$new();
+    location = $location;
+    http = _$httpBackend_;
+    controller = $controller('ProfileController', {
+      $scope: scope
+    });
+  }));
+  
+  afterEach(function() {
+    http.verifyNoOutstandingExpectation();
+    http.verifyNoOutstandingRequest();
+  });
+  
+  
+  it('should emit an event when load method is called', function(done) {
+    
+  });
+});
