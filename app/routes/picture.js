a2c83604ed3ffc10563dc98443d07f06e62868e8
kadusjc <kadusjc@yahoo.com.br>
2014-09-28 12:13:02 -0300

    mudando model

--- app/routes/picture.js
+++ app/routes/picture.js
@@ -3,8 +3,9 @@ module.exports = function(app) {
   var picture = app.controllers.picture;
 
   app.get('/picture/:id', picture.load);
-  app.post('/picture/upload/firstImage', picture.uploadImage);  //TODO use the same method an route for 3 imagems
   app.get('/picture/:id/image?picture=foto1', picture.displayImage)
   app.get('/picture/:id/image?picture=foto2', picture.displayImage)
   app.get('/picture/:id/image?picture=foto3', picture.displayImage)
+
+  app.post('/picture/upload/firstImage', picture.uploadImage);  //TODO use the same method an route for 3 imagems
 };
