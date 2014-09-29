446a5c69abc0d9242003d1453d6e315f893180e3
Carlos Eduardo Ferreiro Corrêa <kadusjc@yahoo.com.br>
2014-09-29 07:54:37 -0300

    Update picture.js
    Correcao

--- app/routes/picture.js
+++ app/routes/picture.js
@@ -3,9 +3,7 @@ module.exports = function(app) {
   var picture = app.controllers.picture;
 
   app.get('/picture/:id', picture.load);
-  app.get('/picture/:id/image?picture=foto1', picture.displayImage)
-  app.get('/picture/:id/image?picture=foto2', picture.displayImage)
-  app.get('/picture/:id/image?picture=foto3', picture.displayImage)
-
+  app.get('/picture/:id/image', picture.displayImage);
+  
   app.post('/picture/upload/firstImage', picture.uploadImage);  //TODO use the same method an route for 3 imagems
 };
