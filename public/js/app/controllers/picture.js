d1c85caf8bb94864eb2cbd6ea52509627950b4fb
Carlos Eduardo Ferreiro Corrêa <kadusjc@yahoo.com.br>
2014-09-29 14:28:31 -0300

    Update picture.js

--- public/js/app/controllers/picture.js
+++ public/js/app/controllers/picture.js
@@ -4,9 +4,6 @@ agencia
   .controller('PictureController', ['$scope','User','Picture',
   function($scope, User, Picture) {
     
-    $scope.firstImage = '/picture/'+$scope.currentUser.id+'/image?picture=foto1';
-    $scope.secondImage = '/picture/'+$scope.currentUser.id+'/image?picture=foto2';
-    $scope.thirdImage = '/picture/'+$scope.currentUser.id+'/image?picture=foto3';
 
 }]);
 
